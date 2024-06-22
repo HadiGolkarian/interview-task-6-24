0c171aaa37aef4337d05dbd841930a076f021583
Hadi Golkarian <HadiGolkarian@Gmail.com>
2024-06-22 20:41:44 +0400

    Card Component Tests

--- src/app/shared/components/card/card.component.spec.ts
+++ src/app/shared/components/card/card.component.spec.ts
@@ -1,23 +1,45 @@
+import { Component, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { CardComponent } from './card.component';
 
+@Component({
+  template: `
+    <app-card>
+      <div class="card-content">
+        <p>content of the card</p>
+      </div>
+    </app-card>
+  `,
+})
+class TestHostComponent {}
+
 describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
+  let cardComponent: CardComponent;
+  let cardContent: DebugElement;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CardComponent]
-    })
-    .compileComponents();
+      declarations: [TestHostComponent],
+      imports: [CardComponent],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(CardComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    cardComponent = fixture.debugElement.query(
+      By.directive(CardComponent)
+    ).componentInstance;
+    cardContent = fixture.debugElement.query(By.css('.card-content'));
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(cardComponent).toBeTruthy();
+  });
+
+  it('should project content', () => {
+    const cardText = cardContent.query(By.css('p')).nativeElement.textContent;
+
+    expect(cardText).toContain('content of the card');
   });
 });
