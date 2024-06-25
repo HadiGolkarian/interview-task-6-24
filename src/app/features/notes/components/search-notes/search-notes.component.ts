1bd8d29ff10ad6b65d16bb4b4038132b464cdeec
Hadi Golkarian <HadiGolkarian@Gmail.com>
2024-06-26 03:00:42 +0400

    Bug Fix

--- src/app/features/notes/components/search-notes/search-notes.component.ts
+++ src/app/features/notes/components/search-notes/search-notes.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faRemove, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { debounceTime, map, Subject } from 'rxjs';
+import { debounceTime, map, Subject, Subscription } from 'rxjs';
 import { NotesService } from '../../services/notes.service';
 
 @Component({
@@ -11,21 +11,26 @@ import { NotesService } from '../../services/notes.service';
   templateUrl: './search-notes.component.html',
   styleUrl: './search-notes.component.sass',
 })
-export class SearchNotesComponent {
+export class SearchNotesComponent implements OnInit, OnDestroy {
   faSearch = faSearch;
   faRemove = faRemove;
   currentSearchTerm: string = '';
   private searchText = new Subject<string>();
+  searchTextSubscription?: Subscription;
 
   constructor(private noteService: NotesService) {}
 
   ngOnInit(): void {
-    this.searchText
+    this.searchTextSubscription = this.searchText
       .pipe(
         debounceTime(1000),
         map((searchTerm: string) => this.noteService.searchNotes(searchTerm))
       )
       .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.searchTextSubscription?.unsubscribe();
   }
 
   handleInputChange(e: Event) {
